2ca0c6166581e5634efb987aa52d61e32649cb05
saltyfishie98-fedora <teugene888@gmail.com>
2023-08-27 23:06:36 +0800

    change battery to urgency to critical

--- src/main.rs
+++ src/main.rs
@@ -31,23 +31,64 @@ fn main() {
     {
         match acpi::log_to_file(&res) {
             Ok(_) => (),
-            Err(_) => log::error!("acpi.rs resource failure!"),
+            Err(_) => log::error!("acpi.rs resource error!"),
         };
 
-        let mut command = Command::new("notify-send");
+        let mut batt_low_command = Command::new("notify-send");
 
-        let notify = command.args([
+        let batt_low_notify = batt_low_command.args([
+            "-u",
+            "critical",
             "Battery Low",
             format!("Battery charge at {}%!", current.percent).as_str(),
         ]);
 
-        match notify.status() {
+        match batt_low_notify.status() {
             Ok(res) => {
                 if !res.success() {
                     log::error!("Command failed with exit code: {:?}", res.code());
                 }
             }
             Err(err) => log::error!("Error executing command: {}", err),
+        }
+    }
+
+    if current.status != old.status {
+        match acpi::log_to_file(&res) {
+            Ok(_) => (),
+            Err(_) => log::error!("acpi.rs resource error!"),
+        };
+
+        let mut charging_command = Command::new("notify-send");
+
+        if current.status == acpi::ChargeStatus::Charging {
+            let charging_notify = charging_command
+                .args(["Battery Status Update", "The battery has started charging!"]);
+
+            match charging_notify.status() {
+                Ok(res) => {
+                    if !res.success() {
+                        log::error!("Command failed with exit code: {:?}", res.code());
+                    }
+                }
+                Err(err) => log::error!("Error executing command: {}", err),
+            }
+        }
+
+        if current.status == acpi::ChargeStatus::Discharging
+            || current.status == acpi::ChargeStatus::NotCharging
+        {
+            let charging_notify = charging_command
+                .args(["Battery Status Update", "The battery has stopped charging!"]);
+
+            match charging_notify.status() {
+                Ok(res) => {
+                    if !res.success() {
+                        log::error!("Command failed with exit code: {:?}", res.code());
+                    }
+                }
+                Err(err) => log::error!("Error executing command: {}", err),
+            }
         }
     }
 }
