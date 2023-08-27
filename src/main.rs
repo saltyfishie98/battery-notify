0e1678987f41090091444f77bca5bd6f58be983b
saltyfishie98-fedora <teugene888@gmail.com>
2023-08-27 21:56:37 +0800

    temporary implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::process::Command;
+
 mod acpi;
 
 fn main() {
+    env_logger::init();
+
     let res = acpi::call();
 
     let old_res = acpi::from_file();
@@ -8,7 +12,7 @@ fn main() {
     let old = match acpi::parse(&old_res) {
         Ok(out) => out,
         Err(_) => {
-            eprintln!("Error parsing acpi data");
+            log::error!("Error parsing previous acpi data");
             return;
         }
     };
@@ -16,18 +20,31 @@ fn main() {
     let current = match acpi::parse(&res) {
         Ok(out) => out,
         Err(_) => {
-            eprintln!("Error parsing acpi data");
+            log::error!("Error parsing current acpi data");
             return;
         }
     };
 
-    if current.percent != old.percent {
-        println!("old: {}", old);
-        println!("\nnew: {}", current);
-
+    if current.percent != old.percent && current.percent <= 20 {
         match acpi::log_to_file(&res) {
             Ok(_) => (),
-            Err(e) => eprintln!("Error logging to file: {}", e),
+            Err(_) => log::error!("acpi.rs resource failure!"),
+        };
+
+        let mut command = Command::new("notify-send");
+
+        let notify = command.args([
+            "Battery Low",
+            format!("Battery charge at {}%!", current.percent).as_str(),
+        ]);
+
+        match notify.status() {
+            Ok(res) => {
+                if !res.success() {
+                    log::error!("Command failed with exit code: {:?}", res.code());
+                }
+            }
+            Err(err) => log::error!("Error executing command: {}", err),
         }
     }
 }
