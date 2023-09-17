cbe0f5d391eb00b019e841b2949ea9c645682562
saltyfishie-fedora <teugene888@gmail.com>
2023-09-17 11:27:07 +0800

    refactor: organised battery stuff to own file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use notify::{Config, Event, PollWatcher, RecursiveMode, Watcher};
-use regex::Regex;
 use std::{
     future::Future,
     path::Path,
@@ -8,181 +7,16 @@ use std::{
 };
 use tokio::sync::mpsc::{channel, Receiver};
 
-const POWER_SUPPLY_PATH: &str = "/sys/class/power_supply";
-
-#[derive(Debug)]
-enum ChargeStatus {
-    Charging,
-    Discharging,
-    NotCharging,
-    Unknown,
-}
-
-impl From<&str> for ChargeStatus {
-    fn from(value: &str) -> Self {
-        match value {
-            "Charging" => Self::Charging,
-            "Not charging" => Self::NotCharging,
-            "Discharging" => Self::Discharging,
-            _ => Self::Unknown,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Battery {
-    pub id: u32,
-    pub percent: u32,
-    pub status: ChargeStatus,
-}
-
-#[derive(Debug)]
-enum BatteryError {
-    BatteryDoesNotExist,
-}
-
-impl Battery {
-    fn get_live_percent(id: u32) -> Result<u32, BatteryError> {
-        let percent_file_string = format!("{POWER_SUPPLY_PATH}/BAT{id}/capacity");
-        let percent_file = Path::new(&percent_file_string);
-
-        let string_from_file = match std::fs::read_to_string(percent_file) {
-            Ok(out) => out,
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::NotFound {
-                    return Err(BatteryError::BatteryDoesNotExist);
-                } else {
-                    log::error!("{:?}", e);
-                    panic!();
-                }
-            }
-        };
-
-        match string_from_file.replace("\n", "").parse::<u32>() {
-            Ok(out) => return Ok(out),
-            Err(err) => {
-                log::error!("error: {err}");
-                panic!();
-            }
-        };
-    }
-
-    fn get_live_status(id: u32) -> Result<ChargeStatus, BatteryError> {
-        let status_file_string = format!("{POWER_SUPPLY_PATH}/BAT{id}/status");
-        let status_file = Path::new(&status_file_string);
-
-        let string_from_file = match std::fs::read_to_string(status_file) {
-            Ok(out) => out,
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::NotFound {
-                    return Err(BatteryError::BatteryDoesNotExist);
-                } else {
-                    log::error!("{:?}", e);
-                    panic!();
-                }
-            }
-        };
-
-        Ok(string_from_file.replace("\n", "").as_str().into())
-    }
-}
-
-struct Batteries {
-    pub entry: Vec<Battery>,
-}
-
-impl Default for Batteries {
-    fn default() -> Self {
-        let dir_entries = match std::fs::read_dir(Path::new(POWER_SUPPLY_PATH)) {
-            Ok(out) => out,
-            Err(e) => {
-                log::error!("{}", e);
-                panic!()
-            }
-        };
-
-        let power_supplies: Vec<String> = dir_entries
-            .map(|entry| {
-                let path = match entry {
-                    Ok(ent) => ent.path(),
-                    Err(err) => {
-                        log::error!("{}", err);
-                        panic!();
-                    }
-                };
-
-                match path.file_name() {
-                    Some(out) => out.to_string_lossy().to_string(),
-                    None => {
-                        log::error!("Sys power supply directory is empty!");
-                        panic!();
-                    }
-                }
-            })
-            .collect();
-
-        let batt_dirs: Vec<String> = power_supplies
-            .into_iter()
-            .map(|path_string| {
-                if path_string.contains("BAT") {
-                    path_string
-                } else {
-                    "".to_string()
-                }
-            })
-            .filter(|s| !s.is_empty())
-            .collect();
-
-        log::debug!("battery directories: {:?}", batt_dirs);
-
-        let mut entries: Vec<Battery> = batt_dirs
-            .into_iter()
-            .map(|path_string| {
-                let re = Regex::new(r"\d+").unwrap();
-                let id: u32 = re
-                    .find_iter(&path_string)
-                    .into_iter()
-                    .map(|id_string| id_string.as_str().parse::<u32>().unwrap())
-                    .next()
-                    .unwrap();
-
-                let percent = Battery::get_live_percent(id).unwrap();
-                let status = Battery::get_live_status(id).unwrap();
-
-                Battery {
-                    id,
-                    percent,
-                    status,
-                }
-            })
-            .collect();
-
-        entries.sort_by(|a, b| {
-            if a.id < b.id {
-                std::cmp::Ordering::Less
-            } else if a.id > b.id {
-                std::cmp::Ordering::Greater
-            } else {
-                std::cmp::Ordering::Equal
-            }
-        });
-
-        log::debug!("batteries data: {:?}", entries);
-
-        Self { entry: entries }
-    }
-}
-
-type BatteryState = Arc<Mutex<Battery>>;
+mod battery;
 
 struct State {
-    battery_state: BatteryState,
+    battery_state: Arc<Mutex<battery::Battery>>,
 }
 
 impl State {
     fn new() -> Self {
         State {
-            battery_state: Arc::new(Mutex::new(Batteries::default().entry.remove(0))),
+            battery_state: Arc::new(Mutex::new(battery::Batteries::default().entry.remove(0))),
         }
     }
 
@@ -202,7 +36,7 @@ impl State {
         async move {
             let (mut watcher, mut rx) = helper::watcher()?;
 
-            let battery_percent_file = format!("{POWER_SUPPLY_PATH}/BAT0/capacity");
+            let battery_percent_file = battery::percent_path(0);
             let percent_path = Path::new(battery_percent_file.as_str());
 
             watcher.watch(percent_path.as_ref(), RecursiveMode::NonRecursive)?;
@@ -210,7 +44,7 @@ impl State {
             while let Some(res) = rx.recv().await {
                 match res {
                     Ok(_) => {
-                        let percent = Battery::get_live_percent(0).unwrap();
+                        let percent = battery::Battery::get_live_percent(0).unwrap();
                         let mut battery_state = percent_mutex.lock().unwrap();
                         log::info!("battery percent update: {percent}%");
                         battery_state.percent = percent;
@@ -228,7 +62,7 @@ impl State {
         async move {
             let (mut watcher, mut rx) = helper::watcher()?;
 
-            let battery_status_file = format!("{POWER_SUPPLY_PATH}/BAT0/status");
+            let battery_status_file = battery::status_path(0);
             let status_path = Path::new(battery_status_file.as_str());
 
             watcher.watch(status_path.as_ref(), RecursiveMode::NonRecursive)?;
@@ -236,7 +70,7 @@ impl State {
             while let Some(res) = rx.recv().await {
                 match res {
                     Ok(_) => {
-                        let status = Battery::get_live_status(0).unwrap();
+                        let status = battery::Battery::get_live_status(0).unwrap();
                         let mut battery_state = status_mutex.lock().unwrap();
                         log::info!("battery status update: {:?}", status);
                         battery_state.status = status;
